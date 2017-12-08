5275da808756fce831f154fa3aeed37e3e3e8143
1377195627 <1377195627@qq.com>
2017-12-08 23:46:26 +0800

    完善博客安装

--- install.go
+++ install.go
@@ -5,27 +5,39 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"fmt"
 	"crypto/md5"
+	"os"
 )
 
 func Install(config Config) error {
-	sqlserver := fmt.Sprintf("%s:%s@%s:%s/",config.Db.Username,config.Db.Password,config.Db.Address,config.Db.Port)
+	sqlserver := fmt.Sprintf("%s:%s@tcp(%s:%s)/",config.Db.Username,config.Db.Password,config.Db.Address,config.Db.Port)
 	db,err := sql.Open(config.Db.Driver,sqlserver)
 	if err!= nil {
 		return err
 	}
 
-	db.Exec("CREATE DATABASE "+config.Db.Dbname+" IF NOT EXIST")
+	fmt.Println(db.Exec("CREATE DATABASE "+config.Db.Dbname))
 
 	db.Exec("use "+config.Db.Dbname)
 
 	_,err=db.Exec(`CREATE TABLE article(
-		name VARCHAR(20) NOT NULL
-		md_name VARCHAR(20) NOT NULL
-		tag CHAR(10) NOT NULL
-		create_time DATETIME NOT NULL
-		edit_time DATEIME NOT NULL
+		name VARCHAR(20) NOT NULL,
+		uuid VARCHAR(20) NOT NULL,
+		tag CHAR(10) NOT NULL,
+		create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		edit_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`)
 
+	if _,err = os.Stat("article"); err != nil {
+		if os.IsNotExist(err) {
+			err=os.MkdirAll("article", 0775)
+		}
+	}
+
+	if err!= nil {
+		return err
+	}
+
+	err = AddArticle("世界，您好！","test","欢迎使用goblog。这是您的第一篇文章。编辑或删除它，然后开始写作吧！",config)
 	if err!= nil {
 		return err
 	}
