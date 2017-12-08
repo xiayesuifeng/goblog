3b269b2f03e60d626fe642237834080c56bc669c
1377195627 <1377195627@qq.com>
2017-12-09 00:02:15 +0800

    添加删除文章功能

--- article.go
+++ article.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"github.com/satori/go.uuid"
 	"io/ioutil"
+	"os"
 )
 
 func AddArticle(name string,tag string,context string,config Config) error {
@@ -28,5 +29,22 @@ func UpdateArticle(name string,tag string,context string,config Config) error{
 }
 
 func DelArticle(name string,config Config) error{
-	return nil
+	sqlserver := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",config.Db.Username,config.Db.Password,config.Db.Address,config.Db.Port,config.Db.Dbname)
+	db,err := sql.Open(config.Db.Driver,sqlserver)
+	if err!= nil {
+		return err
+	}
+
+	var md_uuid string
+	row:=db.QueryRow("SELECT uuid FROM article WHERE name=?",name)
+	err = row.Scan(&md_uuid)
+	if err!= nil {
+		return err
+	}
+
+	if err=os.Remove("article/"+md_uuid+".md"); err!= nil{
+		return err
+	}
+	_,err = db.Exec("DELETE FROM article WHERE name=?",name)
+	return err
 }
