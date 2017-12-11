682ea60dd2ebe7579551aa71887067b64d5c34a0
1377195627 <1377195627@qq.com>
2017-12-11 20:47:37 +0800

    完善代码

--- article.go
+++ article.go
@@ -1,22 +1,22 @@
 package goblog
 
 import (
-	"fmt"
-	"database/sql"
 	"github.com/satori/go.uuid"
 	"io/ioutil"
 	"os"
 )
 
-func AddArticle(name string,tag string,context string,config Config) error {
-	sqlserver := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",config.Db.Username,config.Db.Password,config.Db.Address,config.Db.Port,config.Db.Dbname)
-	db,err := sql.Open(config.Db.Driver,sqlserver)
-	if err!= nil {
-		return err
-	}
+type Article struct {
+	Name string
+	Uuid string
+	Tag string
+	CreateTime int64
+	EditTime int64
+}
 
+func AddArticle(name string,tag string,context string) error {
 	md_uuid := uuid.NewV1().String()
-	_,err = db.Exec(`INSERT INTO article (name,uuid,tag) VALUES (?,?,?)`,name,md_uuid,tag)
+	_,err := DB.Exec(`INSERT INTO article (name,uuid,tag) VALUES (?,?,?)`,name,md_uuid,tag)
 	if err!= nil {
 		return err
 	}
@@ -24,20 +24,14 @@ func AddArticle(name string,tag string,context string,config Config) error {
 	return ioutil.WriteFile("article/"+md_uuid+".md",[]byte(context),0644)
 }
 
-func UpdateArticle(name string,tag string,context string,config Config) error{
+func UpdateArticle(name string,tag string,context string) error{
 	return nil
 }
 
-func DelArticle(name string,config Config) error{
-	sqlserver := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",config.Db.Username,config.Db.Password,config.Db.Address,config.Db.Port,config.Db.Dbname)
-	db,err := sql.Open(config.Db.Driver,sqlserver)
-	if err!= nil {
-		return err
-	}
-
+func DelArticle(name string) error{
 	var md_uuid string
-	row:=db.QueryRow("SELECT uuid FROM article WHERE name=?",name)
-	err = row.Scan(&md_uuid)
+	row:=DB.QueryRow("SELECT uuid FROM article WHERE name=?",name)
+	err := row.Scan(&md_uuid)
 	if err!= nil {
 		return err
 	}
@@ -45,6 +39,6 @@ func DelArticle(name string,config Config) error{
 	if err=os.Remove("article/"+md_uuid+".md"); err!= nil{
 		return err
 	}
-	_,err = db.Exec("DELETE FROM article WHERE name=?",name)
+	_,err = DB.Exec("DELETE FROM article WHERE name=?",name)
 	return err
 }
