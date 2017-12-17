f4e41c69f3c902bf3becc1e23dd4237242a4f6e5
1377195627 <1377195627@qq.com>
2017-12-17 23:55:48 +0800

    修改获取token返回的ttl时间

--- token/token.go
+++ token/token.go
@@ -24,5 +24,5 @@ func NewToken(ttl time.Duration) Token{
 	h.Write([]byte(strconv.FormatInt(i, 10)))
 	fmt.Println(i)
 	token := fmt.Sprintf("%x", h.Sum([]byte("goblog")))
-	return Token{Token:token,Ttl:ttl.Nanoseconds()}
+	return Token{Token:token,Ttl:int64(ttl.Seconds())}
 }
