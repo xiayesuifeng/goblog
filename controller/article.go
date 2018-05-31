e33175d551b14577556522f89d35c8c63e2067af
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 23:28:00 +0800

    done add article function

--- controller/article.go
+++ controller/article.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/1377195627/goblog/article"
+)
 
 type Article struct {
 }
@@ -21,7 +24,29 @@ func (a *Article) Gets(ctx *gin.Context) {
 }
 
 func (a *Article) Post(ctx *gin.Context) {
+	var data struct {
+		article.Article
+		Context string `json:"context" binding:"required"`
+	}
 
+	if err := ctx.ShouldBind(&data);err!=nil{
+		ctx.JSON(200,gin.H{
+			"code":100,
+			"message":err.Error(),
+		})
+		return
+	}
+
+	if err:=article.AddArticle(data.Title,data.Tag,data.CategoryId,data.Context);err!=nil{
+		ctx.JSON(200,gin.H{
+			"code":100,
+			"message":err.Error(),
+		})
+	}else{
+		ctx.JSON(200,gin.H{
+			"code":0,
+		})
+	}
 }
 
 func (a *Article) Put(ctx *gin.Context) {
