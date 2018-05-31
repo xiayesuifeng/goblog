064cdbd919e47e0e7018bbafbeae7196198722d4
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 21:40:13 +0800

    add tag controller

---
+++ controller/tag.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Tag struct {
+}
+
+func (t *Tag) Get(ctx *gin.Context)  {
+
+}
+
+func (t *Tag) Gets(ctx *gin.Context)  {
+
+}
