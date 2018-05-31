e271559d4e8b097a784ad83e95d7f4b9ed7cade3
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 19:57:39 +0800

    add article package

---
+++ article/article.go
@@ -0,0 +1,34 @@
+package article
+
+import (
+	"github.com/1377195627/goblog/category"
+	"github.com/jinzhu/gorm"
+	"github.com/1377195627/goblog/database"
+	"github.com/satori/go.uuid"
+	"io/ioutil"
+)
+
+type Article struct {
+	gorm.Model
+	Title    string `gorm:"type:varchar(100);unique" json:"title"`
+	Tag      string `json:"tag"`
+	Uuid     string
+	CategoryId uint `json:"category_id"`
+}
+
+func AddArticle(title, tag string, categoryId uint,context string) error {
+	_,err := category.GetCategory(categoryId)
+	if err != nil {
+		return err
+	}
+
+	md_uuid := uuid.NewV1().String()
+
+	article := Article{Title:title,Tag:tag,Uuid:md_uuid,CategoryId:categoryId}
+	db := database.Instance()
+	if err:= db.Create(&article).Error;err!=nil{
+		return err
+	}
+
+	return ioutil.WriteFile("data/article/"+md_uuid+".md", []byte(context), 0644)
+}
