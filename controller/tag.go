bf4b4093309d90bfd4780d1730e6f538151ab066
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 23:27:19 +0800

    done get tags

--- controller/tag.go
+++ controller/tag.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/1377195627/goblog/database"
 )
 
 type Tag struct {
@@ -12,5 +13,26 @@ func (t *Tag) Get(ctx *gin.Context)  {
 }
 
 func (t *Tag) Gets(ctx *gin.Context)  {
+	db := database.Instance()
+	rows,err :=db.Table("articles").Select("tag").Rows()
+	if err != nil {
+		ctx.JSON(200,gin.H{
+			"code":100,
+			"message":err.Error(),
+		})
+		return
+	}
 
+	tags := make([]string,0)
+
+	for rows.Next() {
+		var tag string
+		rows.Scan(&tag)
+		tags = append(tags, tag)
+	}
+
+	ctx.JSON(200, gin.H{
+		"code": 0,
+		"tags": tags,
+	})
 }
