6942e3b69f78fc9b3253ee372f68d13429d23959
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 20:05:37 +0800

    add router

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/1377195627/goblog/article"
+	"github.com/1377195627/goblog/category"
+	"github.com/1377195627/goblog/controller"
 	"github.com/1377195627/goblog/core"
+	"github.com/1377195627/goblog/database"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"log"
 	"os"
 	"strconv"
-	"github.com/1377195627/goblog/db"
 )
 
 var (
@@ -17,6 +20,42 @@ var (
 )
 
 func main() {
+	router := gin.Default()
+
+	store := sessions.NewCookieStore([]byte("goblog"))
+	router.Use(sessions.Sessions("goblog-session", store))
+
+	apiRouter := router.Group("/api")
+
+	apiRouter.POST("/login")
+
+	{
+		category := &controller.Category{}
+		apiRouter.GET("/category", category.Gets)
+		apiRouter.GET("/category/:id", category.Get)
+		apiRouter.POST("/category", category.Post)
+		apiRouter.PUT("/category/:id", category.Put)
+		apiRouter.DELETE("/category/:id", category.Delete)
+	}
+
+	apiRouter.GET("/tag")
+	apiRouter.GET("/tag/:tag")
+
+	{
+		article := &controller.Article{}
+		apiRouter.GET("/article", article.Gets)
+		apiRouter.GET("/article/category/:category", article.GetByCategory)
+		apiRouter.GET("/article/name/:name", article.Get)
+		apiRouter.GET("/article/uuid/:uuid", article.GetByUuid)
+		apiRouter.POST("/article", article.Post)
+		apiRouter.PUT("/article", article.Put)
+		apiRouter.DELETE("/article/:id", article.Delete)
+	}
+
+	router.Run(":" + strconv.Itoa(*port))
+}
+
+func init() {
 	flag.Parse()
 
 	err := core.ParseConf("config.json")
@@ -28,25 +67,7 @@ func main() {
 		log.Panicln(err)
 	}
 
-	db.Init()
-
-	router := gin.Default()
-
-	store := sessions.NewCookieStore([]byte("goblog"))
-	router.Use(sessions.Sessions("goblog-session", store))
-
-	apiRouter := router.Group("/api")
-
-	apiRouter.POST("/login")
-
-	apiRouter.GET("/tag")
-	apiRouter.GET("/tag/:tag")
-
-	apiRouter.GET("/article/name/:name")
-	apiRouter.GET("/article/uuid/:uuid")
-	apiRouter.POST("/article")
-	apiRouter.PUT("/article")
-	apiRouter.DELETE("/article/:name")
-
-	router.Run(":" + strconv.Itoa(*port))
+	db := database.Instance()
+	db.AutoMigrate(&category.Category{})
+	db.AutoMigrate(&article.Article{})
 }
