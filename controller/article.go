636821ca7d7299991be01141afdcd4f648e49f90
xiayesuifeng <gdigrgg@gmail.com>
2018-06-01 00:17:03 +0800

    dong Put and Delete function

--- controller/article.go
+++ controller/article.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/1377195627/goblog/article"
+	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 type Article struct {
@@ -29,30 +30,77 @@ func (a *Article) Post(ctx *gin.Context) {
 		Context string `json:"context" binding:"required"`
 	}
 
-	if err := ctx.ShouldBind(&data);err!=nil{
-		ctx.JSON(200,gin.H{
-			"code":100,
-			"message":err.Error(),
+	if err := ctx.ShouldBind(&data); err != nil {
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
 		})
 		return
 	}
 
-	if err:=article.AddArticle(data.Title,data.Tag,data.CategoryId,data.Context);err!=nil{
-		ctx.JSON(200,gin.H{
-			"code":100,
-			"message":err.Error(),
+	if err := article.AddArticle(data.Title, data.Tag, data.CategoryId, data.Context); err != nil {
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
 		})
-	}else{
-		ctx.JSON(200,gin.H{
-			"code":0,
+	} else {
+		ctx.JSON(200, gin.H{
+			"code": 0,
 		})
 	}
 }
 
 func (a *Article) Put(ctx *gin.Context) {
+	param := ctx.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
 
+	}
+
+	var data struct {
+		article.Article
+		Context string `json:"context" binding:"required"`
+	}
+
+	if err := ctx.ShouldBind(&data); err != nil {
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	if err:=article.EditArticle(uint(id), data.CategoryId, data.Title, data.Tag, data.Context); err != nil {
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
+		})
+	} else {
+		ctx.JSON(200, gin.H{
+			"code": 0,
+		})
+	}
 }
 
 func (a *Article) Delete(ctx *gin.Context) {
+	param := ctx.Param("id")
+	id,err :=strconv.Atoi(param)
+	if err != nil {
+		ctx.JSON(200, gin.H{
+			"code":      100,
+			"message": "id must integer",
+		})
+		return
+	}
 
+	if err:= article.DeleteArticle(id);err!=nil{
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
+		})
+	}else{
+		ctx.JSON(200, gin.H{
+			"code":    0,
+		})
+	}
 }
