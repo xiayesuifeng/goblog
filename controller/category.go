1f506fac8898d1891d031743b17380a89ec06d90
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 20:50:19 +0800

    done put function

--- controller/category.go
+++ controller/category.go
@@ -65,7 +65,44 @@ func (c *Category) Get(ctx *gin.Context) {
 }
 
 func (c *Category) Put(ctx *gin.Context) {
+	param := ctx.Param("id")
+	id,err :=strconv.Atoi(param)
+	if err != nil {
+		ctx.JSON(200, gin.H{
+			"code":      100,
+			"message": "id must integer",
+		})
+		return
+	}
 
+	data,err := category.GetCategory(uint(id))
+	if err != nil {
+		ctx.JSON(200,gin.H{
+			"code":100,
+			"message":"category not found",
+		})
+		return
+	}
+
+	ctx.Bind(&data)
+	if data.Name == "" {
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": "name is null",
+		})
+		return
+	}
+
+	if err:=data.SetName(data.Name);err!=nil{
+		ctx.JSON(200, gin.H{
+			"code":    100,
+			"message": err.Error(),
+		})
+	}else{
+		ctx.JSON(200, gin.H{
+			"code":    0,
+		})
+	}
 }
 
 func (c *Category) Delete(ctx *gin.Context) {
