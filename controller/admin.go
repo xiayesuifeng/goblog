337e4905664c0bcc489e9193ed4419d79cdb598f
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 21:40:06 +0800

    add admin controller

---
+++ controller/admin.go
@@ -0,0 +1,14 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type Admin struct {
+}
+
+func (a *Admin) Login(ctx *gin.Context) {
+
+}
+
+func (a *Admin) Logout(ctx *gin.Context) {
+
+}
