0c56ac1121de93aed47ffc3c99b39ff576608498
xiayesuifeng <gdigrgg@gmail.com>
2018-05-31 19:57:00 +0800

    add category

---
+++ category/category.go
@@ -0,0 +1,34 @@
+package category
+
+import (
+	"github.com/jinzhu/gorm"
+	"errors"
+	"github.com/1377195627/goblog/database"
+)
+
+type Category struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(100);unique" json:"name"`
+}
+
+func AddCategory(name string) error {
+	db := database.Instance()
+	return db.Create(&Category{Name:name}).Error
+}
+
+func GetCategorys() []Category {
+	db := database.Instance()
+	categorys := make([]Category,0)
+	db.Find(&categorys)
+	return categorys
+}
+
+func GetCategory(id uint) (Category,error) {
+	db := database.Instance()
+	category := Category{}
+	if db.Find(&category,id).RecordNotFound() {
+		return category, errors.New("category not found")
+	}
+
+	return category,nil
+}
