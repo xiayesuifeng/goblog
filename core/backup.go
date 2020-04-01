e45735c1bb627ef6ca49d10a1f7d8a890162cf49
xiayesuifeng <gdigrgg@gmail.com>
2020-04-01 15:21:02 +0800

    add DumpPostgresDatabase function

--- core/backup.go
+++ core/backup.go
@@ -3,6 +3,7 @@ package core
 import (
 	"errors"
 	"io/ioutil"
+	"os"
 	"os/exec"
 )
 
@@ -21,4 +22,24 @@ func DumpMysqlDatabase() error {
 	}
 
 	return ioutil.WriteFile(Conf.DataDir+"/backup/database.sql", data, 0664)
+}
+
+func DumpPostgresDatabase() error {
+	path, err := exec.LookPath("pg_dump")
+	if err != nil {
+		return err
+	}
+
+	db := Conf.Db
+
+	cmd := exec.Command(path, "-h", db.Address, "-p", db.Port, "-U", db.Username, "-f", Conf.DataDir+"/backup/database.sql", db.Dbname)
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "PGPASSWORD="+db.Password)
+
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.New(string(data))
+	}
+
+	return nil
 }
